1a246e83b684a8aedd850436db0855b1c3a1a234
BetaCat_HA <wangjianyu2311077@gmail.com>
2025-04-25 22:58:57 +0800

    优化：商户侧首页数据统计的描述

--- src/views/welcome/PaymentTable.vue
+++ src/views/welcome/PaymentTable.vue
@@ -23,11 +23,11 @@ onBeforeMount(getData);
   <div
     class="bg-white px-[20px] mt-[20px] rounded-[4px] dark:bg-[#141414] flex-1"
   >
-    <div class="font-bold text-[20px] leading-[60px]">收益统计</div>
+    <div class="font-bold text-[20px] leading-[60px]">{{ isAdmin ? "收益统计" : "支出统计"}}</div>
     <div class="flex gap-[20px] flex-col items-center">
       <el-card class="flex-1 w-[80%]">
         <div class="flex items-center gap-20 mb-[20px]">
-          <div class="h-[100%] w-[100px]">临保今日收益</div>
+          <div class="h-[100%] w-[100px]">{{ isAdmin ? "临保今日收益" : "临保今日支出" }}</div>
           <CountTo
             :endVal="data.todayTemporaryAmount ?? 0"
             :startVal="0"
@@ -36,7 +36,7 @@ onBeforeMount(getData);
           />
         </div>
         <div class="flex items-center gap-20">
-          <div class="h-[100%] w-[100px]">临保总收益</div>
+          <div class="h-[100%] w-[100px]">{{ isAdmin ? "临保总收益" : "临保总支出" }}</div>
           <CountTo
             :endVal="data.totalTemporaryAmount ?? 0"
             :startVal="0"
--- src/views/welcome/Statistics.vue
+++ src/views/welcome/Statistics.vue
@@ -31,17 +31,17 @@ onBeforeMount(getData);
 
 <template>
   <div class="bg-white px-[20px] mt-[20px] rounded-[4px] dark:bg-[#141414] flex-1">
-    <div class="font-bold text-[20px] leading-[60px]">收益统计</div>
+    <div class="font-bold text-[20px] leading-[60px]">{{ isAdmin ? "收益统计" : "支出统计" }}</div>
     <el-empty v-if="isDataEmpty" description="暂无数据" class="h-[400px] w-[100%]" />
     <div v-if="!isDataEmpty" class="flex gap-[20px] flex-col items-center">
       <el-card class="flex-1 w-[80%]">
         <div class="flex items-center gap-20 mb-[20px]">
-          <div class="h-[100%] w-[100px]">临保今日收益</div>
+          <div class="h-[100%] w-[100px]">{{ isAdmin ? "临保今日收益" : "临保今日支出" }}</div>
           <CountTo :endVal="data.todayTemporaryAmount" :startVal="0" class="text-2xl flex-1 text-right" suffix="元"
             :decimals="2" />
         </div>
         <div class="flex items-center gap-20">
-          <div class="h-[100%] w-[100px]">临保总收益</div>
+          <div class="h-[100%] w-[100px]">{{ isAdmin ? "临保总收益" : "临保总支出" }}</div>
           <CountTo :endVal="data.totalTemporaryAmount" :startVal="0" class="text-2xl flex-1 text-right" suffix="元"
             :decimals="2" />
         </div>
